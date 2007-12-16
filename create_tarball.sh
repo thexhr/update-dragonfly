385e2b7db7ad8c2c15ff52cfa8f58fec0190d0c3
matthias <matthias>
2007-12-16 18:29:19 +0000

    Script to generate a distribution ready tarball

---
+++ create_tarball.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+d=`mktemp -d /tmp/updfly.XXXX`
+ud=update-dragonfly
+cvsid=matthias@globus.mathematik.uni-marburg.de:/cvs
+remote=schmidtm@login.mathematik.uni-marburg.de:.www/update_dragonfly/
+
+cd /tmp
+
+if [ -d $ud ]; then
+	rm -I -rf $ud
+	rm $ud.tar*
+fi
+
+echo "Check out latest source"
+cvs -d $cvsid co $ud || return
+echo "Create distrib package"
+mv $ud/client/* $d
+rm -r $ud
+mv $d $ud
+rm -r $ud/CVS
+rm -f $ud/*.swp
+echo "Create tarball"
+tar cf $ud.tar $ud || return
+gzip -9 $ud.tar || return
+echo ""
+echo "scp /tmp/$ud.tar.gz $remote"
